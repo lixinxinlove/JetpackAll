13c5cfbc0a0f95c473e38ccc3d83991ac652dc4a
lixinxinlove <895330766@qq.com>
2020-11-12 09:56:34 +0800

    返回后台

--- app/src/main/java/com/lixinxinlove/all/MainActivity.kt
+++ app/src/main/java/com/lixinxinlove/all/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.lixinxinlove.all
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.MotionEvent
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.lixinxinlove.all.adapter.TypeAdapter
 import com.lixinxinlove.all.base.BaseActivity
+
 
 class MainActivity : BaseActivity() {
 
@@ -25,4 +28,16 @@ class MainActivity : BaseActivity() {
     override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
         return super.dispatchTouchEvent(ev)
     }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            val home = Intent(Intent.ACTION_MAIN)
+            home.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+            home.addCategory(Intent.CATEGORY_HOME)
+            startActivity(home)
+            return true
+        }
+        return super.onKeyDown(keyCode, event)
+    }
+
 }
