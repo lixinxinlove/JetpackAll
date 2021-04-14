5137bec84d019a630b7bffb61d90c1831702e5ee
咖啡男 <oncwnuGjxKKCPtiJt3XEudfd3S0M@git.weixin.qq.com>
2021-04-14 18:49:38 +0800

    DeepLinkActivity.kt

---
+++ app/src/main/java/com/lixinxinlove/all/JetpackDeepLinkModule.kt
@@ -0,0 +1,8 @@
+package com.lixinxinlove.all
+
+import com.airbnb.deeplinkdispatch.DeepLinkModule
+
+
+@DeepLinkModule
+class JetpackDeepLinkModule {
+}
--- app/src/main/java/com/lixinxinlove/all/activity/GotoLinkActivity.kt
+++ app/src/main/java/com/lixinxinlove/all/activity/GotoLinkActivity.kt
@@ -2,11 +2,13 @@ package com.lixinxinlove.all.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.airbnb.deeplinkdispatch.DeepLink
 import com.lixinxinlove.all.R
 
-class FloatWinfowActivity : AppCompatActivity() {
+@DeepLink("https://dribbble.com/shots/{id}")
+class GotoLinkActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_float_winfow)
+        setContentView(R.layout.activity_goto_link)
     }
 }
