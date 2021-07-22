eb889db1ddd1e8c2cf6d6af6f1f2f1a7c0d0d3e5
lixinxin <lixinxin@eventmosh.com>
2021-07-22 14:57:40 +0800

    添加opencv使用demo

--- app/src/main/java/com/lixinxinlove/all/opencv/OpenCvDemo1Activity.kt
+++ app/src/main/java/com/lixinxinlove/all/opencv/OpenCvDemo1Activity.kt
@@ -1,24 +1,29 @@
 package com.lixinxinlove.all.opencv
 
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import com.lixinxinlove.all.R
+import com.lixinxinlove.all.base.BaseActivity
 import org.opencv.android.OpenCVLoader
 import org.opencv.android.Utils
 import org.opencv.core.Core
 import org.opencv.core.Mat
+import org.opencv.imgproc.Imgproc
 
 /**
  * openCV
  */
-class OpenCvDemo1Activity : AppCompatActivity() {
+class OpenCvDemo1Activity : BaseActivity() {
 
 
     private lateinit var image: ImageView
+    private lateinit var image4: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,11 +34,10 @@ class OpenCvDemo1Activity : AppCompatActivity() {
             Log.e("CppActivity", "initDebug失败")
         }
         image = findViewById(R.id.image3)
+        image4 = findViewById(R.id.image4)
 
 
         var dstmat = Mat()
-
-
         val srcmat1 = Utils.loadResource(this, R.drawable.icon0)
         val srcmat2 = Utils.loadResource(this, R.drawable.icon1)
 
@@ -41,14 +45,36 @@ class OpenCvDemo1Activity : AppCompatActivity() {
         findViewById<Button>(R.id.btn1).setOnClickListener {
 
             Core.bitwise_and(srcmat1, srcmat2, dstmat)
-            val bitmap = Bitmap.createBitmap(dstmat.width(), dstmat.height(), Bitmap.Config.ARGB_8888)
+            val bitmap =
+                Bitmap.createBitmap(dstmat.width(), dstmat.height(), Bitmap.Config.ARGB_8888)
             Utils.matToBitmap(dstmat, bitmap)
-
             image.setImageBitmap(bitmap)
 
 
+
+            srcmat1.release()
+            srcmat2.release()
+            dstmat.release()
         }
 
-
     }
+
+
+    fun imageToGray(view: View) {
+
+        val startTime = System.currentTimeMillis()
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.icon0)
+        val src = Mat()
+        val dst = Mat()
+        Utils.bitmapToMat(bitmap, src)
+        Imgproc.cvtColor(src, dst, Imgproc.COLOR_BGRA2GRAY)
+        Utils.matToBitmap(dst, bitmap)
+        image4.setImageBitmap(bitmap)
+        src.release()
+        dst.release()
+        val endTime = System.currentTimeMillis()
+        Log.e(TAG, "耗时=${endTime - startTime}")
+    }
+
+
 }
