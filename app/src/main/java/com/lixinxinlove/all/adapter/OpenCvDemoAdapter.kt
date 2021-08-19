1739ccfa5b6386c16049390eb4314032c97ec829
lixinxin <lixinxin@eventmosh.com>
2021-08-19 10:30:05 +0800

    升级

--- app/src/main/java/com/lixinxinlove/all/adapter/OpenCvDemoAdapter.kt
+++ app/src/main/java/com/lixinxinlove/all/adapter/OpenCvDemoAdapter.kt
@@ -6,8 +6,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.lixinxinlove.all.R
-import com.lixinxinlove.all.opencv.OpenCvDemo1Activity
-import com.lixinxinlove.all.opencv.OpenCvDemo2Activity
+import com.lixinxinlove.all.opencv.*
 import javax.inject.Inject
 
 /**
@@ -24,6 +23,9 @@ class OpenCvDemoAdapter @Inject constructor() :
     init {
         mData.add("基础Api使用")
         mData.add("图片区域切割")
+        mData.add("滤镜")
+        mData.add("人脸识别")
+        mData.add("视频")
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LeeViewViewHolder {
@@ -48,6 +50,21 @@ class OpenCvDemoAdapter @Inject constructor() :
                 "图片区域切割" -> {
                     it.context.startActivity(
                         Intent(it.context, OpenCvDemo2Activity::class.java)
+                    )
+                }
+                "滤镜" -> {
+                    it.context.startActivity(
+                        Intent(it.context, OpenCvDemo3Activity::class.java)
+                    )
+                }
+                "人脸识别" -> {
+                    it.context.startActivity(
+                        Intent(it.context, OpenCvDemo4Activity::class.java)
+                    )
+                }
+                "视频" -> {
+                    it.context.startActivity(
+                        Intent(it.context, OpenCvDemo5Activity::class.java)
                     )
                 }
             }
