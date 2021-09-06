f337cd2f20bd66c160b26670653f275383719010
lixinxin <lixinxin@eventmosh.com>
2021-09-06 17:04:56 +0800

    添加 金额转中文大写

--- app/src/main/java/com/lixinxinlove/all/activity/TTSActivity.java
+++ app/src/main/java/com/lixinxinlove/all/activity/TTSActivity.java
@@ -10,6 +10,7 @@ import android.widget.Toast;
 
 import com.lixinxinlove.all.R;
 import com.lixinxinlove.all.base.BaseActivity;
+import com.lixinxinlove.all.util.MoneyUtil;
 
 import java.util.Locale;
 
@@ -28,25 +29,30 @@ public class TTSActivity extends BaseActivity implements OnInitListener {
         //初始化TTS
         tts = new TextToSpeech(this, this);
         //获取控件
-        speechText = (TextView)findViewById(R.id.speechTextView);
-        speechButton = (Button)findViewById(R.id.speechButton);
+        speechText = (TextView) findViewById(R.id.speechTextView);
+        speechButton = (Button) findViewById(R.id.speechButton);
         //为button添加监听
         speechButton.setOnClickListener(v -> {
 
-            tts.speak(speechText.getText().toString(), TextToSpeech.QUEUE_FLUSH, null);
+
+            String chineseMoney = MoneyUtil.toChinese("98.45");
+            speechText.setText(chineseMoney);
+            String order = "收到" + chineseMoney;
+
+            tts.speak(order, TextToSpeech.QUEUE_FLUSH, null);
         });
 
     }
 
     @Override
-    public void onInit(int status){
+    public void onInit(int status) {
         // 判断是否转化成功
-        if (status == TextToSpeech.SUCCESS){
+        if (status == TextToSpeech.SUCCESS) {
             //默认设定语言为中文，原生的android貌似不支持中文。
             int result = tts.setLanguage(Locale.CHINESE);
-            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED){
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
                 Toast.makeText(TTSActivity.this, "Language is not available", Toast.LENGTH_SHORT).show();
-            }else{
+            } else {
                 //不支持中文就将语言设置为英文
                 tts.setLanguage(Locale.US);
             }
